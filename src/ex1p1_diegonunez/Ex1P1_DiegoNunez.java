549cc1667f0289732e8921dcdf0ad9ca11081cc9
Danifuzz <diego_nunez@unitec.edu>
2025-11-14 16:00:51 -0600

    cambio2

--- src/ex1p1_diegonunez/Ex1P1_DiegoNunez.java
+++ src/ex1p1_diegonunez/Ex1P1_DiegoNunez.java
@@ -5,6 +5,7 @@
 package ex1p1_diegonunez;
 
 import static ex1p1_diegonunez.Ex1P1_DiegoNunez.WordReplace;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 /**
@@ -25,22 +26,29 @@ public class Ex1P1_DiegoNunez {
                 System.out.println("Ingrese la cadena:");
 
                 String cadena = entrada.next();
-                
+
                 System.out.println("Ingrese la cadena a buscar:");
-                
+
                 String search = entrada.next();
-                
+
                 System.out.println("Ingrese la palabra a reemplazar:");
-                
+
                 String replace = entrada.next();
                 WordReplace(cadena, search, replace);
 
             } else if (ejercicio == 2) {
-
+                System.out.println("Ingrese una cadena de texto:");
+                String cadena = entrada.next();
+                Vecinos(cadena);
             } else if (ejercicio == 3) {
-
+                int x = entrada.nextInt();
+                Triforce(x);
             } else if (ejercicio == 4) {
-
+                System.out.println("ingrese cadena 1");
+                String cadena1 = entrada.next();
+                System.out.println("Ingrese cadena 2");
+                String cadena2 = entrada.next();
+                Anagram(cadena1, cadena2);
             }
         } while (1 < 0);
 
@@ -49,7 +57,7 @@ public class Ex1P1_DiegoNunez {
     public static String WordReplace(String cadena, String search, String replace) {
         int cont = 0;
         String acum = "";
-        for (int i = 0; i < cadena.length()-1; i++) {
+        for (int i = 0; i < cadena.length() - 1; i++) {
 
             if (cadena.charAt(i) == search.charAt(cont)) {
                 int posicion = i;
@@ -65,9 +73,81 @@ public class Ex1P1_DiegoNunez {
             }
         }
 
- 
         System.out.println(replace);
-
         return (replace);
     }
-}
+
+    public static String Vecinos(String cadena) {
+        
+        SecureRandom random = new SecureRandom();
+        int caracter = 0;
+        int r = random.nextInt(2);  
+        String acum = "";
+        if (r==0) {
+            for (int i = 0; i < cadena.length()-1; i++) {
+               if ( cadena.charAt(i) >= 97 && cadena.charAt(i) <=122) {
+                   caracter = (char) cadena.charAt(i) - 122;
+                   if (caracter < 0) {
+                       caracter = caracter*-1;
+                   }
+                   caracter = caracter + 97;
+               }
+               char cara = (char) caracter;
+               acum = acum + cara;
+               
+            } 
+        } else {
+            for (int i = 0; i < cadena.length()-1; i++) {
+                if ((char) cadena.charAt(i) != 122) {
+                    
+                
+                acum = acum + cadena.charAt(i) + 1;
+                } else {
+                    acum = acum + 97;
+                }
+            }
+        } 
+        System.out.println(acum);
+        
+        int name = 97-122;
+        return (cadena);
+    }
+    public static int Triforce(int x) {
+        int cont = 0;
+        int cont2 = 0;
+        if (x%2==1) {
+        for (int i = 0; i < x; i++) {
+            
+            while (cont2 <= x) {
+                System.out.print(" ");
+                cont2++;
+            }
+            System.out.print("*");
+            System.out.println(" ");
+            cont = cont + 1;
+        }
+        }
+        return (x);
+    }
+    public static boolean Anagram(String cadena1, String cadena2) {
+        boolean anagrama = false;
+        int acum1 = 0;
+        int acum2 = 0;
+        String acum = "";
+        if (cadena1.length()==cadena2.length()) {
+        for (int i = 0; i < cadena1.length(); i++) {
+            acum1 = acum1 + (char) cadena1.charAt(i);
+            acum2 = acum2 + (char) cadena2.charAt(i);
+        }
+        if (acum1 == acum2) {
+            System.out.println("Es un anagrama.");
+        } else {
+            System.out.println("No es un anagrama");
+        }
+        } else if (cadena1.length()!=cadena2.length()) {
+            System.out.print("no es un anagrama ni es del mismo tamano.");
+        }
+        return (anagrama);
+    }
+    
+} 
